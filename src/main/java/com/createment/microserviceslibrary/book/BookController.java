b546a04ff3dd7bffdbb0db1c8453cbd5079e48e5
Hon Man Wong <honman-wong@hotmail.com>
2023-09-12 13:23:15 +0200

    updated the BookController

--- src/main/java/com/createment/microserviceslibrary/book/BookController.java
+++ src/main/java/com/createment/microserviceslibrary/book/BookController.java
@@ -2,10 +2,9 @@ package com.createment.microserviceslibrary.book;
 
 import jakarta.persistence.criteria.CriteriaBuilder;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/books")
@@ -21,23 +20,16 @@ public class BookController {
 
     @GetMapping
     public List<Book> displayAllBooks() {
-        bookrepository.findAll();
+        return bookrepository.findAll();
     }
 
     @GetMapping("/{bookId}")
     public Book displayBookById(@PathVariable Long bookId) {
-        bookrepository.findById(bookId);
+        return bookrepository.findById(bookId);
     }
 
-    @GetMapping("/lending/{bookId}")
-    public void lendBookById(@PathVariable Long bookId) {
-
+    @PutMapping("/lending/{bookId}/{customer}")
+    public void lendBookById(@PathVariable Long bookId, @PathVariable Customer customer) {
+        bookservice.lendBook(bookId, customer);
     }
-
-    /*
-    ✅ Book microservice should contain an end point for all books, it should also expose if the book is currently lended or not.
-    It should also have an end point to lend a book that accepts the book and customer data.
-    The lending customer can be an existing customer or a new customer. In case of a new customer their details should be passed to Customer microservice.
-     */
-
 }
--- src/main/java/com/createment/microserviceslibrary/book/BookService.java
+++ src/main/java/com/createment/microserviceslibrary/book/BookService.java
@@ -3,9 +3,8 @@ package com.createment.microserviceslibrary.book;
 import jakarta.transaction.Transactional;
 import org.springframework.stereotype.Service;
 
-import java.awt.print.Book;
-
 @Service
+@Transactional
 public class BookService {
     private final BookRepository bookRepository;
 
@@ -13,13 +12,7 @@ public class BookService {
         this.bookRepository = bookRepository;
     }
 
-    @Transactional
-    public void lendBook(Book book, Customer customer) {
+    public void lendBook(Long bookId, Customer customer) {
 
     }
-
-    /*
-    It should also have an end point to lend a book that accepts the book and customer data.
-    The lending customer can be an existing customer or a new customer. In case of a new customer their details should be passed to Customer microservice.
-     */
 }

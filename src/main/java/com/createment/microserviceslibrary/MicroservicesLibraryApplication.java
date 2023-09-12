e5970821e92c01428050fa19155b12fbee4de66e
Chrizzard <chris.scholte@hotmail.com>
2023-09-12 10:01:34 +0200

    My first baby commit

---
+++ src/main/java/com/createment/microserviceslibrary/MicroservicesLibraryApplication.java
@@ -0,0 +1,13 @@
+package com.createment.microserviceslibrary;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MicroservicesLibraryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MicroservicesLibraryApplication.class, args);
+	}
+
+}
